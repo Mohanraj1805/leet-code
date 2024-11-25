d83edadbb1c731d0cdb0ad929327ed032604ca7f
Mohanraj M <160243399+Mohanraj1805@users.noreply.github.com>
2024-11-25 09:57:47 +0530

    Time: 31 ms (5.72%), Space: 11.8 MB (45.00%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        while len(s)>0:
+            l=len(s)
+            s=s.replace("()",'').replace('{}','').replace('[]','')
+            if l==len(s): 
+                return False 
+        return True
