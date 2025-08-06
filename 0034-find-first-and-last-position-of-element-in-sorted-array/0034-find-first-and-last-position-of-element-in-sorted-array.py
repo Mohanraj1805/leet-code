471e3853f29b921dc167df99c496c918f7946125
Mohanraj M <160243399+Mohanraj1805@users.noreply.github.com>
2025-08-06 23:01:47 +0530

    Time: 0 ms (100%), Space: 19.1 MB (20.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,36 +1,36 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
         def left():
             l=0
             r=len(nums)-1
-            index=-1
+            ans=-1
             while l<=r:
                 mid=(l+r)//2
-                if nums[mid]==target:
-                    index=mid
-                    r=mid-1
-
-                elif nums[mid]<target:
+                if nums[mid]< target:
                     l=mid+1
-                elif nums[mid]>target:
+                else:
+                    if nums[mid]==target:
+                        ans=mid
                     r=mid-1
-            return index
+            return ans
         def right():
             l=0
             r=len(nums)-1
-            index=-1
+            ans=-1
             while l<=r:
                 mid=(l+r)//2
-                if nums[mid]==target:
-                    index=mid
-                    l=mid+1
-                elif nums[mid]<target:
-                    l=mid+1
-                elif nums[mid]>target:
+                if nums[mid] >target:
                     r=mid-1
-            return index
-        return [left(),right()]
-
-
-     
+                else:
+                    if nums[mid]==target:
+                        ans=mid
+                    l=mid+1
+            return ans
+        if not nums:
+            return [-1,-1]
+        left_index=left()
+        if left_index==-1:
+            return [-1,-1]
+        right_index=right()
+        return [left_index,right_index]
+        
